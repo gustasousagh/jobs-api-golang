5474e61d8d7a25cb9858d0627430ad89c85ef5b5
gusta <gustavosousa30246@gmail.com>
2024-04-28 23:19:42 -0300

    add logger and add schema

--- main.go
+++ main.go
@@ -1,7 +1,23 @@
 package main
 
-import "github.com/gustasousagh/jobs-api-golang/router"
+import (
+	"fmt"
+
+	"github.com/gustasousagh/jobs-api-golang/config"
+	"github.com/gustasousagh/jobs-api-golang/router"
+)
+
+var (
+	logger *config.Logger
+)
 
 func main() {
+	logger = config.GetLogger("main")
+	err := config.Init()
+	if err != nil {
+		logger.ErrorF("Config: %v", err)
+		fmt.Println(err)
+		return
+	}
 	router.Initialize()
 }
---
+++ schema/opening.go
@@ -0,0 +1,13 @@
+package schema
+
+import "gorm.io/gorm"
+
+type opening struct {
+	gorm.Model
+	Role     string
+	Company  string
+	Location string
+	Remote   bool
+	Link     string
+	Salary   int64
+}
